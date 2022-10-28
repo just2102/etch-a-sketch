92860177f4c4b5655022985830af12348e607898
just2102 <thesixteensaltines@gmail.com>
2022-10-28 15:17:47 +0900

    random rgb color set for each mouse hover, some visual tweaks added

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
 const squareContainer = document.getElementById("container");
+
 function generateSketch() {
-    let squareNumber   =   prompt('How many squares are there in your grid?')
+    let squareNumber   =   prompt('How many squares would you like?')
     while (squareContainer.firstChild) {
             squareContainer.removeChild(squareContainer.firstChild)
         };
     for (let i = 0;i<squareNumber;i++) {
+        function randomRgbColor () {
+        let r = Math.floor(Math.random() * 255+1);
+        let g = Math.floor(Math.random() * 255+1);
+        let b = Math.floor(Math.random() * 255+1);
+        return "rgb(" + r + ',' + g + ',' + b+ ")"
+        }
         let square = document.createElement('div');
         square.className += "square";
         squareContainer.appendChild(square);
         square.onmouseover  =   function() {
-            square.classList.add('active')
+            square.classList.add('active');
+            let color = randomRgbColor();
+            square.style.background    =   color;
         }
     }
 }
